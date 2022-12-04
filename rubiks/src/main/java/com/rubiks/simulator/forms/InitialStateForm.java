fe7b621fb8bb49a1934784176d21d60d056fd7d1
rodrigo-moliveira <rodrigo.moroliveira97@gmail.com>
2022-12-04 22:17:40 +0000

    reformulating repo

---
+++ rubiks/src/main/java/com/rubiks/simulator/forms/InitialStateForm.java
@@ -0,0 +1,26 @@
+package com.rubiks.simulator.forms;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
+public class InitialStateForm {
+	public void run()
+	{
+		final JFrame parent = new JFrame();
+	    JButton button = new JButton();
+	
+	    button.setText("Click me to show dialog!");
+	    parent.add(button);
+	    parent.pack();
+	    parent.setVisible(true);
+	
+	    button.addActionListener(new java.awt.event.ActionListener() {
+	        @Override
+	        public void actionPerformed(java.awt.event.ActionEvent evt) {
+	            String name = JOptionPane.showInputDialog(parent,
+	                    "What is your name?", null);
+	        }
+	    });
+	}
+}
